dd05dbb1e041858f68ec5c71eab1d27414bb64c6
unknown <pashish@c22.narola.local>
2014-06-07 14:08:19 +0530

    blog module updates

--- protected/config/console.php
+++ protected/config/console.php
@@ -5,4 +5,49 @@
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'My Console Application',
+	'components'=>array(
+		'db'=>array(
+			'connectionString' => 'mysql:host=localhost;dbname=yiiroot',
+			'emulatePrepare' => true,
+			'username' => 'root',	
+			'password' => '',
+			'charset' => 'utf8',
+			'tablePrefix' => 'tbl_',
+		),
+	),
+	'modules'=>array(
+            #...
+            'user'=>array(
+                # encrypting method (php hash function)
+                'hash' => 'md5',
+
+                # send activation email
+                'sendActivationMail' => true,
+
+                # allow access for non-activated users
+                'loginNotActiv' => false,
+
+                # activate user on registration (only sendActivationMail = false)
+                'activeAfterRegister' => false,
+
+                # automatically login from registration
+                'autoLogin' => true,
+
+                # registration path
+                'registrationUrl' => array('/user/registration'),
+
+                # recovery password path
+                'recoveryUrl' => array('/user/recovery'),
+
+                # login form path
+                'loginUrl' => array('/user/login'),
+
+                # page after login
+                'returnUrl' => array('/user/profile'),
+
+                # page after logout
+                'returnLogoutUrl' => array('/user/login'),
+            ),
+            #...
+        ),
 );
